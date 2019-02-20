30e1694e9cadc980cfccaadc31bd965bc34d1498
Simon Loffler <simon.loffler@acmi.net.au>
2019-02-20 15:50:48 +1100

    Hardcode IP.

--- src/runner.py
+++ src/runner.py
@@ -31,7 +31,9 @@ def get_mac():
 
 # Get IP address
 def get_ip_address():
-  return (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
+  # TOFIX:
+#   return (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
+  return '172.16.80.133'
 ip_address = get_ip_address()
 reader_name = 'nfc-' + ip_address.split('.')[-1]
 
