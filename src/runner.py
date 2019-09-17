cbf233397dd8983cf5fbb0fec54c624812c4ff16
Greg Turner <greg.turner@acmi.net.au>
2019-09-17 19:52:11 +1000

    Re #9 bring across XOS comms code from previous runner code.

--- src/runner.py
+++ src/runner.py
@@ -2,21 +2,35 @@
 
 """ Kiosk IV tap reader running on a Raspberry Pi """
 
+import os
+import re
 import subprocess
 import sys
 from datetime import datetime
-from time import time, sleep
-import re
 from threading import Timer
+from time import sleep, time
+
+import adafruit_dotstar as dotstar
+import requests
+import sentry_sdk
+
 try:
   import board
 except NotImplementedError:
   board = None
-import adafruit_dotstar as dotstar
 
+from utils import log, MAC_ADDRESS, IP_ADDRESS, TZ
 
-BYTE_STRING_RE = r'([0-9a-fA-F]{2}:?)+'
-TAP_OFF_TIMEOUT = 0.5 # seconds
+# Constants defined in environment
+XOS_URL = os.getenv('XOS_URL', 'http://localhost:8888')
+AUTH_TOKEN = os.getenv('AUTH_TOKEN', '')
+LABEL = os.getenv('LABEL')
+SENTRY_ID = os.getenv('SENTRY_ID')
+TAP_OFF_TIMEOUT = float(os.getenv('TAP_OFF_TIMEOUT', '0.5')) # seconds
+DEVICE_NAME = os.getenv('DEVICE_NAME')
+READER_MODEL = os.getenv('READER_MODEL')
+READER_NAME = DEVICE_NAME or 'nfc-' + IP_ADDRESS.split('.')[-1]
+
 
 LEDS_COLOUR_DEFAULT = (254, 254, 254)  # White
 LEDS_COLOUR_SUCCESS = (0, 254, 0)  # Green
@@ -27,6 +41,9 @@ LEDS_FADE_BRIGHTNESS_STEP = 0.01
 LEDS_FADE_TIME_BETWEEN_STEP = 0.005
 LEDS_TIME_BETWEEN_FAILED_BLINKS = 0.3
 
+BYTE_STRING_RE = r'([0-9a-fA-F]{2}:?)+'
+
+
 RASPBERRY_PI = False
 
 if RASPBERRY_PI:
@@ -34,6 +51,8 @@ if RASPBERRY_PI:
 else:
   CMD = ["./idtech_debug"]
 
+# Setup Sentry
+sentry_sdk.init(SENTRY_ID)
 
 
 def byte_string_to_lens_id(byte_string):
@@ -46,12 +65,11 @@ def byte_string_to_lens_id(byte_string):
   return "".join(byte_string.strip().split(":")[1:]).lower()
 
 
-def log(*args):
-    print("%s: " % datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 
-      *args
-    )
 
 class LedsManager:
+  """
+  LedsManager manages the state of the reader's LEDs.
+  """
   def __init__(self):
     # LED init
     # Using a DotStar Digital LED Strip with 12 LEDs connected to hardware SPI
@@ -95,6 +113,11 @@ class LedsManager:
 
 
 class TapManager:
+  """
+  TapManager processes continuous messages from the idtech C code, in order to detect tap on and tap off events.
+  Tap On events send a message to a nominated XOS address, and light up the reader's LEDs.
+  Tap Off events are currently only logged.
+  """
   def __init__(self):
     # Tap init
     self.last_id = None
@@ -102,15 +125,48 @@ class TapManager:
     self.tap_off_timer = None
     self.leds = LedsManager()
 
+  def send_tap(self, id):
+    """refer to docs.python-requests.org for implementation examples"""
+    params={
+      'nfc_tag': {
+        'uid':id
+      },
+      'tap_datetime': datetime.now(TZ).isoformat(),
+      'label': LABEL,
+      'data': {
+        'nfc_reader': {
+          'mac_address': MAC_ADDRESS,
+          'reader_ip': IP_ADDRESS,
+          'reader_model': READER_MODEL,
+          'reader_name': READER_NAME,
+        }
+      }
+    }
+    headers={'Authorization': 'Token ' + AUTH_TOKEN}
+    try:
+      r=requests.post(url=XOS_URL, json=params, headers=headers)
+      if r.status_code==201:
+        log(r.text)
+        return(0)
+      else:
+        log(r.text)
+        sentry_sdk.capture_message(r.text)
+        return(1)
+    except requests.exceptions.ConnectionError as e:
+      log("Failed to post tap message to %s: %s\n%s" % (XOS_URL, params, str(e)))
+      sentry_sdk.capture_exception(e)
+      return(1)
+
   def tap_on(self):      
     log(" Tap On: ", self.last_id)
     self.leds.success()
+    self.send_tap(self.last_id)
 
   def tap_off(self):
     log("Tap Off: ", self.last_id)
     self.last_id = None
 
-  def reset_tap_off_timer(self):
+  def _reset_tap_off_timer(self):
     # reset the tap-off timer for this ID
     if self.tap_off_timer:
       self.tap_off_timer.cancel()
@@ -119,6 +175,9 @@ class TapManager:
     self.tap_off_timer.start()
 
   def read_id(self, id):
+    """
+    This is called continuously while an NFC tag is present.
+    """
     if id != self.last_id:
       # send a tap-off message if needed
       if self.tap_off_timer and self.tap_off_timer.is_alive():
@@ -126,7 +185,7 @@ class TapManager:
         self.tap_off()
       self.last_id = id
       self.tap_on()
-    self.reset_tap_off_timer()
+    self._reset_tap_off_timer()
 
 
 def main():
@@ -134,7 +193,6 @@ def main():
   print("KioskIV Lens Reader")
 
   shell = True
-  count = 0
   popen = subprocess.Popen(CMD, shell=shell, stdout=subprocess.PIPE)
 
   tap_manager = TapManager()
