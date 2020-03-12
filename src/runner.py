4be32c93075f7687010808c6f9028a45caf76b89
David Amores <davidamores3@gmail.com>
2020-03-12 12:54:36 +1100

    #37 Passing lint

--- src/runner.py
+++ src/runner.py
@@ -3,15 +3,12 @@
 """ Kiosk IV tap reader running on a Raspberry Pi """
 
 import json
-import math
 import os
 import re
 import subprocess
-import sys
 from datetime import datetime
-from threading import Timer, Thread, Event
+from threading import Thread, Timer
 from time import sleep, time
-from copy import deepcopy
 
 import requests
 import sentry_sdk
@@ -21,19 +18,23 @@ from utils import IP_ADDRESS, IS_OSX, MAC_ADDRESS, TZ, envtotuple, log
 
 try:
     import adafruit_dotstar as dotstar
-except ModuleNotFoundError: # this doesn't compile install
-    dotstar = None
+
+    dotstar_imported = True  # pylint: disable=invalid-name
+except ModuleNotFoundError:  # this doesn't compile install
+    dotstar_imported = False  # pylint: disable=invalid-name
 
 try:
     import board
+
+    board_imported = None  # pylint: disable=invalid-name
 except (NotImplementedError, ModuleNotFoundError):
-    board = None
+    board_imported = None  # pylint: disable=invalid-name
 
 
 # Constants defined in environment. Changes here should be documented in README.
 
 # whether to use the DEBUG version of the idtech C code
-DEBUG = os.getenv('DEBUG', 'false').lower() == "true"
+DEBUG = os.getenv('DEBUG', 'false').lower() == 'true'
 TARGET_API_ENDPOINT = os.getenv('TARGET_API_ENDPOINT', 'http://localhost:8000/api/')
 TARGET_TAPS_ENDPOINT = os.getenv('TARGET_TAPS_ENDPOINT', f'{TARGET_API_ENDPOINT}taps/')
 AUTH_TOKEN = os.getenv('AUTH_TOKEN', '')
@@ -42,69 +43,74 @@ SENTRY_ID = os.getenv('SENTRY_ID')
 
 DEVICE_NAME = os.getenv('DEVICE_NAME')
 READER_MODEL = os.getenv('READER_MODEL')
-READER_NAME = DEVICE_NAME or 'nfc-' + IP_ADDRESS.split('.')[-1] # not user-settable
+READER_NAME = DEVICE_NAME or 'nfc-' + IP_ADDRESS.split('.')[-1]  # not user-settable
 
-# seconds
-TAP_OFF_TIMEOUT = float(os.getenv('TAP_OFF_TIMEOUT', '0.5'))
+TAP_OFF_TIMEOUT = float(os.getenv('TAP_OFF_TIMEOUT', '0.5'))  # seconds
 LEDS_BRIGHTNESS = float(os.getenv('LEDS_BRIGHTNESS', '1.0'))
-LEDS_BREATHE_TIME = float(os.getenv('LEDS_BREATHE_TIME', '5.0')) # seconds to cycle between IN and OUT colours.
-LEDS_BREATHE_COLOUR_OUT = envtotuple('LEDS_BREATHE_COLOUR_OUT', "36,26,10")  # Dim warm white
-LEDS_BREATHE_COLOUR_IN = envtotuple('LEDS_BREATHE_COLOUR_IN',  "95,70,26")  # Medium warm white
-LEDS_SUCCESS_COLOUR = envtotuple('LEDS_SUCCESS_COLOUR', "255,238,202")  # Bright warm white
-LEDS_FAILED_COLOUR = envtotuple('LEDS_FAILED_COLOUR', "137,0,34")  # Medium red
-LEDS_SIGNAL_TIMES = envtotuple('LEDS_SIGNAL_TIMES', "0.3,0.5,0.6") # up ramp_on, auto-hold (if needed), down ramp_on
-LEDS_IN_STRING = int(os.getenv('LEDS_IN_STRING', '12')) # Number of LEDs to light up
+# seconds to cycle between IN and OUT colours.
+LEDS_BREATHE_TIME = float(os.getenv('LEDS_BREATHE_TIME', '5.0'))
+LEDS_BREATHE_COLOUR_OUT = envtotuple('LEDS_BREATHE_COLOUR_OUT', '36,26,10')  # Dim warm white
+LEDS_BREATHE_COLOUR_IN = envtotuple('LEDS_BREATHE_COLOUR_IN', '95,70,26')  # Medium warm white
+LEDS_SUCCESS_COLOUR = envtotuple('LEDS_SUCCESS_COLOUR', '255,238,202')  # Bright warm white
+LEDS_FAILED_COLOUR = envtotuple('LEDS_FAILED_COLOUR', '137,0,34')  # Medium red
+# up ramp_on, auto-hold (if needed), down ramp_on
+LEDS_SIGNAL_TIMES = envtotuple('LEDS_SIGNAL_TIMES', '0.3,0.5,0.6')
+LEDS_IN_STRING = int(os.getenv('LEDS_IN_STRING', '12'))  # Number of LEDs to light up
 
 if IS_OSX:
-    FOLDER = "../bin/mac/"
+    FOLDER = '../bin/mac/'
 else:
-    FOLDER = "../bin/arm_32/"
+    FOLDER = '../bin/arm_32/'
 
 if DEBUG:
-    CMD = ["./idtech_debug"] # this will run silently
+    CMD = ['./idtech_debug']  # this will run silently
 else:
-    CMD = ["./idtech"] # this will produce 2 beeps from the reader when run
+    CMD = ['./idtech']  # this will produce 2 beeps from the reader when run
 
 # Setup Sentry
 sentry_sdk.init(SENTRY_ID)
 
-app = Flask(__name__)
-tap_manager = None
+app = Flask(__name__)  # pylint: disable=invalid-name
 
 
 class LEDControllerThread(Thread):
+    # pylint: disable=too-many-instance-attributes
     """
-    A thread that cycles the LED colours as a gaussian-ish 'breathe' state, until interrupted with a ramp_on in/ramp_on out to another colour.
+    A thread that cycles the LED colours as a gaussian-ish 'breathe' state,
+    until interrupted with a ramp_on in/ramp_on out to another colour.
     """
 
     @staticmethod
-    def ease(t, b, c, d):
+    def ease(current_time, beginning_value, value_change, duration):
         # Penner's easeInCubic
-        # t: current time, b: beginning value, c: change in value, d: duration
-        t = t / d
-        r = c * t * t * t + b
-        return r
+        current_time /= duration
+        return value_change * (current_time ** 3) + beginning_value
 
     def __init__(self):
         super(LEDControllerThread, self).__init__()
-        self.current_colour = [0,0,0] # the colour LEDs will be set to each fram
-        self.breathe_colour = [0,0,0] # the current colour of the breathing animation
+        self.current_colour = [0, 0, 0]  # the colour LEDs will be set to each fram
+        self.breathe_colour = [0, 0, 0]  # the current colour of the breathing animation
 
-        self.ramp_target_colour = [0,0,0] # the target colour of the ramping animation
+        self.ramp_target_colour = [0, 0, 0]  # the target colour of the ramping animation
         self.ramp_target = 0.0
         self.ramp_duration = None
         self.ramp_time0 = None
         self.blocked_by = None
 
-        if board and dotstar:
-            self.LEDS = dotstar.DotStar(board.SCK, board.MOSI, LEDS_IN_STRING, brightness=LEDS_BRIGHTNESS)
+        if board_imported and dotstar_imported:
+            self.leds = dotstar.DotStar(
+                board.SCK,
+                board.MOSI,
+                LEDS_IN_STRING,
+                brightness=LEDS_BRIGHTNESS
+            )
         else:
-            self.LEDS = None
+            self.leds = None
 
     def set_leds(self, colour):
-        colour = [min(max(int(i), 0),255) for i in colour]
-        if self.LEDS:
-            self.LEDS.fill((*colour, LEDS_BRIGHTNESS))
+        colour = [min(max(int(i), 0), 255) for i in colour]
+        if self.leds:
+            self.leds.fill((*colour, LEDS_BRIGHTNESS))
         else:
             print("Setting LEDs to %s" % list(colour))
 
@@ -123,56 +129,65 @@ class LEDControllerThread(Thread):
         self.ramp_target = 0.0
         self.ramp_time0 = time()
 
-    def _calculate_breathe_colour(self, t):
-        # cubic approximation to gauss from http://www.iquilezles.org/www/articles/functions/functions.htm
-        x = t % LEDS_BREATHE_TIME
+    def _calculate_breathe_colour(self, time_):
+        # pylint: disable=invalid-name
+        # cubic approximation to gauss from
+        # http://www.iquilezles.org/www/articles/functions/functions.htm
+        x = time_ % LEDS_BREATHE_TIME
         # returns a ramp_on value between 0 (breathe out) and 1 (breathe in)
         w = LEDS_BREATHE_TIME / 2
-        if (x<w):
+        if x < w:
             x = (-x + w) / w
         else:
             x = (x - w) / w
         ramp_val = 1.0 - x * x * (3.0 - 2.0 * x)
         for i in range(3):
-            self.breathe_colour[i] = LEDS_BREATHE_COLOUR_OUT[i] + ramp_val * (LEDS_BREATHE_COLOUR_IN[i] - LEDS_BREATHE_COLOUR_OUT[i])
+            self.breathe_colour[i] = LEDS_BREATHE_COLOUR_OUT[i] + ramp_val * \
+                (LEDS_BREATHE_COLOUR_IN[i] - LEDS_BREATHE_COLOUR_OUT[i])
 
     def _calculate_ramp_proportion(self):
         """
-        If we're ramping up or down, calculate where we are in that process (from 0 to 1 and back again, depending on the value of target_proprotion)
+        If we're ramping up or down, calculate where we are in that process
+        (from 0 to 1 and back again, depending on the value of target_proprotion)
         """
         if self.ramp_time0 is not None:
             # we're ramping
-            t = time() - self.ramp_time0
-            if t >= self.ramp_duration:
+            time_ = time() - self.ramp_time0
+            if time_ >= self.ramp_duration:
                 # we're done ramping
                 self.ramp_time0 = None
                 return self.ramp_target
-            else:
-                # we're not done ramping
-                if self.ramp_target > 0.5:
-                    # we're ramping up
-                    return LEDControllerThread.ease(t, 0.0, self.ramp_target, self.ramp_duration)
-                else:
-                    # we're ramping down
-                    return LEDControllerThread.ease(t, 1.0, self.ramp_target-1.0, self.ramp_duration)
-        else:
-            # we're not ramping
-            return self.ramp_target
+
+            # we're not done ramping
+            if self.ramp_target > 0.5:
+                # we're ramping up
+                return LEDControllerThread.ease(
+                    time_, 0.0, self.ramp_target, self.ramp_duration
+                )
+
+            # we're ramping down
+            return LEDControllerThread.ease(
+                time_, 1.0, self.ramp_target - 1.0, self.ramp_duration
+            )
+
+        # we're not ramping
+        return self.ramp_target
 
     def run(self):
-        t0 = time()
+        start_time = time()
         while True:
-            t = time() - t0
-            self._calculate_breathe_colour(t)
+            time_ = time() - start_time
+            self._calculate_breathe_colour(time_)
             ramp_proportion = self._calculate_ramp_proportion()
 
             # mix the breathe and ramp_on colours according to the current ramp_on amount
             for i in range(3):
-                self.current_colour[i] = ramp_proportion * self.ramp_target_colour[i] + (1.0 - ramp_proportion) * self.breathe_colour[i]
+                self.current_colour[i] = ramp_proportion * self.ramp_target_colour[i] + \
+                    (1.0 - ramp_proportion) * self.breathe_colour[i]
 
             # output the colours
             self.set_leds(self.current_colour)
-            sleep(1.0/60)
+            sleep(1.0 / 60)
 
     def success_on(self):
         self.ramp_on(LEDS_SUCCESS_COLOUR, LEDS_SIGNAL_TIMES[0])
@@ -194,8 +209,12 @@ class LEDControllerThread(Thread):
 
 class TapManager:
     """
-    TapManager processes continuous messages from the idtech C code, in order to detect tap on and tap off events.
-    Tap On events send a message to a nominated XOS address, and light up the reader's LEDs.
+    TapManager processes continuous messages from the idtech C code,
+    in order to detect tap on and tap off events.
+
+    Tap On events send a message to a nominated XOS address,
+    and light up the reader's LEDs.
+
     Tap Off events are currently only logged.
     """
     def __init__(self):
@@ -204,13 +223,14 @@ class TapManager:
         self.last_id_time = time()
         self.tap_off_timer = None
         self.leds = LEDControllerThread()
-        self.leds.start()
 
-    def send_tap(self, id):
-        """refer to docs.python-requests.org for implementation examples"""
-        params={
+    def send_tap(self, lens_id):
+        """
+        Refer to docs.python-requests.org for implementation examples
+        """
+        params = {
             'lens': {
-                'uid':id
+                'uid': lens_id
             },
             'tap_datetime': datetime.now(TZ).isoformat(),
             'label': XOS_LABEL_ID,
@@ -223,28 +243,32 @@ class TapManager:
                 }
             }
         }
-        headers={'Authorization': 'Token ' + AUTH_TOKEN}
+        headers = {'Authorization': 'Token ' + AUTH_TOKEN}
         try:
-            r=requests.post(url=TARGET_TAPS_ENDPOINT, json=params, headers=headers)
-            if r.status_code==201:
-                log(r.text)
-                return(0)
-            elif r.status_code==404:
-                log(r.text)
+            response = requests.post(url=TARGET_TAPS_ENDPOINT, json=params, headers=headers)
+            if response.status_code == 201:
+                log(response.text)
+                return 0
+
+            if response.status_code == 404:
+                log(response.text)
                 self.leds.failed()
-                return(1)
-            else:
-                log(r.text)
-                sentry_sdk.capture_message(r.text)
-                return(1)
-        except requests.exceptions.ConnectionError as e:
-            log("Failed to post tap message to %s: %s\n%s" % (TARGET_TAPS_ENDPOINT, params, str(e)))
-            sentry_sdk.capture_exception(e)
+                return 1
+
+            log(response.text)
+            sentry_sdk.capture_message(response.text)
+            return 1
+
+        except requests.exceptions.ConnectionError as connection_error:
+            log('Failed to post tap message to %s: %s\n%s' % (
+                TARGET_TAPS_ENDPOINT, params, str(connection_error)
+            ))
+            sentry_sdk.capture_exception(connection_error)
             self.leds.failed()
-            return(1)
+            return 1
 
     def tap_on(self):
-        log(" Tap On: ", self.last_id)
+        log(' Tap On: ', self.last_id)
         # turn leds on only if not being used
         if not self.leds.blocked_by:
             self.leds.blocked_by = 'tap'
@@ -269,24 +293,25 @@ class TapManager:
 
     def _byte_string_to_lens_id(self, byte_string):
         """
-        Convert an id of the form 04:04:A5:2C:F2:2A:5E:80 (as output from the C app) to 04a52cf22a5e80 by:
-        - stripping ":" and whitespace
-        - removing first byte
-        - lowercasing
+        Convert an id of the form 04:04:A5:2C:F2:2A:5E:80 (as output from the C app)
+        to 04a52cf22a5e80 by:
+            - stripping ':' and whitespace
+            - removing first byte
+            - lowercasing
         """
-        return "".join(byte_string.strip().split(":")[1:]).lower()
+        return ''.join(byte_string.strip().split(':')[1:]).lower()
 
     def read_line(self, line):
         """
         This is called continuously while an NFC tag is present.
         """
-        id = self._byte_string_to_lens_id(line)
-        if id != self.last_id:
+        lens_id = self._byte_string_to_lens_id(line)
+        if lens_id != self.last_id:
             # send a tap-off message if needed
             if self.tap_off_timer and self.tap_off_timer.is_alive():
                 self.tap_off_timer.cancel()
                 self.tap_off()
-            self.last_id = id
+            self.last_id = lens_id
             self.tap_on()
         self._reset_tap_off_timer()
 
@@ -296,14 +321,15 @@ class TapManager:
         """
         shell = True
         popen = subprocess.Popen(CMD, cwd=FOLDER, shell=shell, stdout=subprocess.PIPE)
-        BYTE_STRING_RE = r'([0-9a-fA-F]{2}:?)+'
+        byte_string_re = r'([0-9a-fA-F]{2}:?)+'
 
         while True:
-            # wait for the next line from the C interface (if there are no NFCs there will be no lines)
-            line = popen.stdout.readline().decode("utf-8")
+            # wait for the next line from the C interface
+            # (if there are no NFCs there will be no lines)
+            line = popen.stdout.readline().decode('utf-8')
 
             # see if it is a tag read
-            if re.match(BYTE_STRING_RE, line):
+            if re.match(byte_string_re, line):
                 # We have an ID.
                 self.read_line(line)
 
@@ -328,7 +354,7 @@ def toggle_lights():
         ramp_time = float(request_data['ramp_time'])
         cross_fade = float(request_data['cross_fade'])
 
-        # ensure we are turning off a previous remote toggle 
+        # ensure we are turning off a previous remote toggle
         # or turning on if nothing is using the leds
         assert (tap_manager.leds.blocked_by == 'remote' and cross_fade == 0.0) or \
             (not tap_manager.leds.blocked_by and cross_fade > 0.0)
@@ -340,7 +366,7 @@ def toggle_lights():
     except (SyntaxError, TypeError, ValueError):
         response = {'error': 'Invalid rgb_value, ramp_time or cross_fade value.'}
         return json.dumps(response), 400
-    except AssertionError as assertion_error:
+    except AssertionError:
         response = {'error': 'Cannot perform action.'}
         return json.dumps(response), 409
 
@@ -348,25 +374,28 @@ def toggle_lights():
 def byte_string_to_lens_id(byte_string):
     """
     Convert 04:04:A5:2C:F2:2A:5E:80 to 04a52cf22a5e80 by:
-    - stripping ":" and whitespace
+    - stripping ':' and whitespace
     - removing first byte
     - lowercasing
     """
-    return "".join(byte_string.strip().split(":")[1:]).lower()
+    return ''.join(byte_string.strip().split(':')[1:]).lower()
+
+
+tap_manager = TapManager()  # pylint: disable=invalid-name
 
 
 def main():
     """Launcher."""
-    print("XOS Lens Reader (KioskIV)")
+    print('XOS Lens Reader (KioskIV)')
 
-    global tap_manager
-    tap_manager = TapManager()
+    tap_manager.leds.start()
 
     # Start thread to read and process taps.
     Thread(target=tap_manager.process_taps).start()
 
     # Start a Flask server to expose /api/lights/ endpoint
     app.run(host='0.0.0.0', port=8082)
+
 
 if __name__ == "__main__":
     main()
--- src/utils.py
+++ src/utils.py
@@ -1,48 +1,51 @@
-import socket
-from datetime import datetime
-import uuid
 import os
-import pytz
 import platform
+import socket
+import uuid
+from datetime import datetime
+
+import pytz
 
 DNS_SERVER = os.getenv('DNS_SERVER', '8.8.8.8')
 DNS_PORT = int(os.getenv('DNS_PORT', '80'))
 TIMEZONE = os.getenv('TIMEZONE', 'Australia/Victoria')
-IS_OSX = platform.system() == "Darwin"
+IS_OSX = platform.system() == 'Darwin'
 
 # Set pytz timezone
 TZ = pytz.timezone(TIMEZONE)
 
 
 def log(*args):
-    print("%s: " % datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 
-        *args
-    )
+    print('%s: ' % datetime.now().strftime('%Y-%m-%d %H:%M:%S'), *args)
+
 
 # Get mac address
 def get_mac_address():
     mac_num = hex(uuid.getnode()).replace('0x', '').upper()
-    mac = ':'.join(mac_num[i : i + 2] for i in range(0, 11, 2))
+    mac = ':'.join(mac_num[i:i + 2] for i in range(0, 11, 2))
     return mac
+
 
 MAC_ADDRESS = get_mac_address()
 
+
 # Get IP address
 def get_ip_address():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect((DNS_SERVER, DNS_PORT))
-    ip_address = s.getsockname()[0]
-    s.close()
+    socket_ = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket_.connect((DNS_SERVER, DNS_PORT))
+    ip_address = socket_.getsockname()[0]
+    socket_.close()
     return ip_address
 
+
 IP_ADDRESS = get_ip_address()
+
 
 def envtotuple(env_name, default=()):
     """
     allow environment values of "1,2,3" to be parsed into (1,2,3)
     """
-    v = os.getenv(env_name)
-    if not v:
-        v = default
-    return tuple([float(x.strip()) for x in v.split(",")])
-
+    env_value = os.getenv(env_name)
+    if not env_value:
+        env_value = default
+    return tuple([float(elem.strip()) for elem in env_value.split(',')])
